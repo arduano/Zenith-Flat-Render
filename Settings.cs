7dcfd0450d5289ef9ad703b2e1462cc263234c7f
arduano <leonid.shchurov@gmail.com>
2019-03-06 01:29:44 +1100

    Flat render

---
+++ Settings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatRender
+{
+    public class Settings
+    {
+        public int firstNote = 0;
+        public int lastNote = 128;
+        public double pianoHeight = 0.2;
+        public double deltaTimeOnScreen = 294.067;
+        public bool sameWidthNotes = true;
+
+        public float noteBrightness = 1;
+
+        public bool tickBased = true;
+    }
+}
